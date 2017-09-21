7de5888d520dee7ff9337ee03eabd8f2daed39ad
D.W <Fake@email.com>
2017-09-20 20:42:55 -0500

    Seek Bar With Square

--- app/src/main/java/com/dez/seekbarlayouts/SeekBarActivity.java
+++ app/src/main/java/com/dez/seekbarlayouts/SeekBarActivity.java
@@ -8,17 +8,22 @@ Purpose:
  */
 package com.dez.seekbarlayouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class SeekBarActivity extends AppCompatActivity {
 
-    //Global vars for widgets
-    SeekBar mSeekBar;        //Identifier Buttom
-    TextView mSeekBarLabel;
+    public static final String EXTRA_SQUARE_SIZE = "com.dez.tapthesquare.square";
+
+    private static final int SQUARE_REQUEST_CODE = 0;
+
 
     //Create a seek bar method
     @Override
@@ -26,42 +31,72 @@ public class SeekBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seek_bar);
 
-        //Initialize vars
-        mSeekBar = (SeekBar) findViewById(R.id.seek_bar);
-        mSeekBarLabel = (TextView) findViewById(R.id.seek_bar_value_label);
+        final TextView seekbarValueTV = (TextView) findViewById(R.id.seek_bar_value_label);
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seek_bar);
 
-        //Set initial TextView value using a format string resource
-        //Set Textview Text to SeekBar's progress
-        mSeekBarLabel.setText(getString(R.string.seekbar_progress, mSeekBar.getProgress()));
+        Button showSquare = (Button) findViewById(R.id.showSquareButton);
 
-        //Set up listenser
-        addSeekBarListener();
-    }
-    //Set up listenr
-    private void addSeekBarListener() {
+        showSquare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        //Set a Listener method on the object mSeekBar
-        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                Intent launchSquareActivity = new Intent(SeekBarActivity.this, SquareActivity.class);
+                //add data to the Intent using putExtra method
+                launchSquareActivity.putExtra(EXTRA_SQUARE_SIZE, seekBar.getProgress());
+
+                startActivityForResult(launchSquareActivity, SQUARE_REQUEST_CODE);
+
+            }
+        });
+
+        int seekbarProgress = seekBar.getProgress();
+
+        seekbarValueTV.setText("The initial SeekBar value is " + seekbarProgress);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mSeekBarLabel.setText(getString(R.string.seekbar_progress, progress));
+                //The onProgressChanged method supplies the current SeekBar Progress
+                seekbarValueTV.setText("The seekbar value is " + progress);
+
             }
 
-            //OnStart will start tracking the Touch of the user
             @Override
-            public void onStartTrackingTouch(SeekBar seeker) {
-                //We don't care about this event in this app, but are required to implement this methoid.
-                //It's ok that nothing happens here, our app doens't need to do anything when this event happens
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Don't need to respond to this event, but still need to implement this method
 
             }
-            //
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //Same as previous method, can leave this empty.
+                //same here
 
             }
-
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+//Need to check what request ended up with this result
+// And need to verify if the result was OK  - and not the user cancelling, or pressing back button
+
+        if (requestCode == SQUARE_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            //Get the data from the Intent. Again, must provide a default
+            boolean tappedSquare = data.getBooleanExtra(SquareActivity.EXTRA_INSIDE_SQUARE, true);
+
+            //Indicate result to user with a toast
+            if (tappedSquare) {
+                Toast.makeText(this, "You tapped the square!@", Toast.LENGTH_LONG).show();
+            } else
+                Toast.makeText(this, "Sorry, you missed the square", Toast.LENGTH_LONG).show();
+
+        }
+
+    }
 }
+
+
+    //https://github.com/minneapolis-edu/Android2417Examples/blob/master/TapTheSquare/app/src/main/java/com/clara/tapthesquare/SeekBarActivity.java
